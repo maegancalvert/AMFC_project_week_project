a3285ccb5152d8ba930b434c472fefd281e7bbad
Maegan Calvert <maegan.calvert@gmail.com>
2021-03-08 16:23:48 -0600

    Changed to generic script

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-fh = '/home/mcalvert/workspace/code/script_generation/SST_demographic_2021_Maegan_working.csv'
+fh = #'file name and location'#
 df = pd.read_csv(fh, delimiter=',')
 
 ### want to drop all rows and columns will all NaN
@@ -10,21 +10,21 @@ df.dropna(axis=1, how= 'all', inplace=True)
 #df.set_index('Subject', inplace=True)
 #print(df)
 
-### find all participants who were not part of Amanda's study and drop the participants who were part of Amanda's study
+### find all participants who were not part of the current study and drop the participants who aren't part of the study
 #for i, row in df.iterrows(): #this is used to iterate over rows using the index (i), but not needed for this script
-df2 = (df.where(df['In_Amanda_HBM_2014'] == 0))
-df2.dropna(axis=0, subset=['In_Amanda_HBM_2014'], inplace=True)
+df2 = (df.where(df['variable'] == 0))
+df2.dropna(axis=0, subset=['variable'], inplace=True)
 #print(df2)
 
 ###find all participants who are older than age 17 and drop participants who are under the age of 18
 df3 = (df2.where(df2['Age'] > 17))
 df3.dropna(axis=0, subset=['Age'], inplace=True)
 
-###find all participants who were part of the UAMS cohort and delete all participants who were part of the Emory cohort
+###find all participants who were part of the current cohort and delete all participants who were not part of the current cohort
 df4 = (df3.where(df['Subject'] > 3200))
 df4.dropna(axis=0, subset=['Subject'], inplace=True)
 
-df4.to_csv('/home/mcalvert/workspace/code/script_generation/SST_demographic_2021_Maegan_revised.csv')
+df4.to_csv #('file name and location')
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 print(df4.count())
 print(df4.describe())
